34f640c381f10510a6b70777579c968a42fbc822
Talitamota <talitamotan@gmail.com>
2018-10-02 17:49:55 -0300

    importa Choice

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
